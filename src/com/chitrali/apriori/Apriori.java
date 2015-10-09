c974972db45ed6b273ca1b9e4b5dd116fdbe1d71
Chitrali Rai <batrachitrali101@gmail.com>
2015-10-09 18:12:23 -0400

    Scanning

--- src/com/chitrali/apriori/Apriori.java
+++ src/com/chitrali/apriori/Apriori.java
@@ -4,43 +4,41 @@ import java.sql.*;
 import java.util.*;
 
 public class Apriori {
-static List<Set<Integer>> dataSet= new ArrayList<Set<Integer>>();
+	
 static	Map<Set<Integer>,Integer> candidateSet = new HashMap<Set<Integer>,Integer>();
 static	Map<Set<Integer>,Integer> frequentSet=new HashMap<Set<Integer>,Integer>();
 static	Map<Set<Integer>,Integer> frequentSetTemp=new HashMap<Set<Integer>,Integer>();
 static Map<Set<Integer>,Set<Set<Integer>>> associationRules=new HashMap<Set<Integer>,Set<Set<Integer>>>();
 static Map<Integer,String> productName=new HashMap<Integer,String>();
 static Map<String,Float> confAR=new HashMap<String,Float>();
-
+static ResultSet res,res2;
+static Connection con;
 static float minsup; 
 static float mincon;
 static int suportCount;
+static int size,k;
+static String CompanyName;
+
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		
-		try
-		{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@prophet.njit.edu:1521:course","cr252","CKHMs7icB");
-			Statement st = con.createStatement();
-			Statement st2=con.createStatement();
-			String sql ="select * from tset1 ORDER BY tid";
-			String sql2="select * from products order by pid";
-			ResultSet res=st.executeQuery(sql);
-			ResultSet res2=st2.executeQuery(sql2);
-						
-			/* read from database and map it to the memory implementation of database 
-			 * while counting the support of each element*/
+		System.out.println("Available Datasets:\nAmazon\nKmart\nWallmart\nSears\nShoprite\n");
+		System.out.println("Please Enter the name of 1 dataset for Apriori Mining");
+		CompanyName=in.nextLine();
 			
+					size=0;
+					k=0;
+					getDataBase();
+					
+			/* Computing C1*/
+			try{
 			while(res.next()){
 				String[] str=res.getString(2).split(",");
-				Set<Integer> row=new HashSet<Integer>();
 				for(int i=0;i<str.length;i++){
 					Integer j=Integer.parseInt(str[i]);
 					Set<Integer> item= new HashSet<Integer>();
 					item.add(j);
-					row.add(j);
 					Integer count=candidateSet.get(item);
 					if(count==null)
 						candidateSet.put(item, 1);
@@ -48,7 +46,7 @@ static int suportCount;
 						candidateSet.put(item,++count);
 					
 				}
-				dataSet.add(row);
+				size++;
 			}
 			// id to product Map
 			while(res2.next()){
@@ -60,39 +58,26 @@ static int suportCount;
 			}
 			con.close();
 			
+			
 			System.out.println("Enter the Minimum Support Count value (In Integer):");
 			suportCount=in.nextInt();
-			minsup=new Float(suportCount)/new Float(dataSet.size());
+			minsup=new Float(suportCount)/new Float(size);
 		    System.out.println("Enter the minimum confidence value (In Decimal):");
 			mincon=in.nextFloat();
 			System.out.println("\n*************************************************");
 			System.out.println(" ***************User Entered Values***************");
-			System.out.println("\nMin support:"+minsup+" \nMin confidence:"+mincon);
+			System.out.println("\nDataset:"+CompanyName+"\nMin support:"+minsup+" \nMin confidence:"+mincon);
 			
-			/* dataSet is the java Implementation of the DataBase*/
-			System.out.println("\n*************************************************\n");
-			System.out.println(" *************The Transaction dataset*************\n");
-			for(Set<Integer> set:dataSet){
-				for(Integer i:set){
-					System.out.print(i+" ");
-				}
-				System.out.println("\n");
-			}
 			
 			System.out.println("\n*************************************************\n");
-			System.out.println(" **************Id to product Mapping**************");
-			for(Integer i: productName.keySet())
-				System.out.println(i+" : "+productName.get(i));
-			
-			System.out.println("\n*************************************************\n");
-			System.out.println("**************************C************************");
+			System.out.println("**************************C1************************");
 			for(Set<Integer> key:candidateSet.keySet()){
 				System.out.println(key + ": =  " + candidateSet.get(key));
 							
 			}
 		
 			frequentSet.clear();
-			prune();
+			pruneBySupport();
 			frequentItemSetGeneration();
 					
 			System.out.println("\n*************************************************\n");
@@ -106,23 +91,35 @@ static int suportCount;
 			System.out.println("\n*************************************************\n");
 			System.out.println(" **************Rule Confidence**************");
 			for(String i: confAR.keySet())
-				System.out.println(i+" : "+confAR.get(i));
-			
-			
-		}
-		catch(Exception e){System.out.println(e);}
+				System.out.println(i+" : "+confAR.get(i));		
+		
 			in.close();
 		
 	}
+	catch(Exception ex){
+		System.out.println(ex);
+	}
+	}
 	
+	/*
+	 * Scanning the database to count the support of each item set
+	 */
 	static int numberofOccurence(Set<Integer> set){
 			int supportCount=0;
 			boolean hasElement;
-			for(Set<Integer> set1:dataSet){
+			getDataBase();
+			try{
+			while(res.next()){
+				String[] str=res.getString(2).split(",");
+				Set<Integer> row=new HashSet<Integer>();
+				for(int i=0;i<str.length;i++){
+					Integer j=Integer.parseInt(str[i]);
+					row.add(j);
+			}
 				int count=0;
 				for(Integer e:set){
 					hasElement=false;
-					for(Integer i: set1){
+					for(Integer i: row){
 						if(e==i){
 							hasElement=true;
 							count++;
@@ -134,15 +131,28 @@ static int suportCount;
 				}
 				if(count==set.size())
 					supportCount++;
+				
 			}
-			
+			}catch(Exception exp){
+				System.out.println(exp);
+			}
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 			return supportCount;
 	}
 	
-	static void prune(){
+	/*
+	 * Pruning the Candidate Set to generate Frequent Item set 
+	 * based on Support value entered by user
+	 */	
+	static void pruneBySupport(){
+		k++;
 		frequentSetTemp.clear();
 		for(Set<Integer> key:candidateSet.keySet()){
-			Float sup=new Float(candidateSet.get(key))/new Float (dataSet.size());
+			Float sup=new Float(candidateSet.get(key))/new Float (size);
 			if(sup>=minsup){
 				frequentSetTemp.put(key,candidateSet.get(key));
 				frequentSet.put(key,candidateSet.get(key));
@@ -150,14 +160,21 @@ static int suportCount;
 		}
 		if(!frequentSetTemp.isEmpty()){
 		System.out.println("\n*************************************************\n");
-		System.out.println("  ************************L************************");
+		System.out.println("  ************************L"+k+"********************");
 		for(Set<Integer> key : frequentSetTemp.keySet()){
 			System.out.println(key + ": =  " + frequentSetTemp.get(key));
 			
 		}
+		}else{
+			System.out.println("\n********************************\n");
+			System.out.println("******No More Frequents Sets******");
 		}
+			
 		
 	}
+	/*
+	 * Generate k+1 Candidate set from k Frequent Item Set
+	 */
 
 	static void frequentItemSetGeneration(){
 		boolean next=true;
@@ -196,19 +213,26 @@ static int suportCount;
 				if(pruneCandidate(s))
 					candidateSet.put(s, numberofOccurence(s));			
 				}
-			
-			System.out.println("\n*************************************************");
+			if(!candidateSet.isEmpty()){
+			System.out.println("\n*************************************************\n");
 			System.out.println("  ***********************C*************************");
 			for(Set<Integer> key:candidateSet.keySet())
 				System.out.println(key + ": =  " + candidateSet.get(key));
+			}
+			else{
+				System.out.println("\n*************************************************\n");
+				System.out.println("  ************No more Candidates*******************");
+			}
 			
-			prune();
+			pruneBySupport();
 			if(frequentSetTemp.size()<=1)
 				next=false;
 			size++;
 		}
 	}
-	
+	/*
+	 * Mining the Association Rules from Frequent Item Sets
+	 */
 	static void mineAssociationRules(){
 		for(Set<Integer> s:frequentSet.keySet()){
 			if(s.size()>1){
@@ -230,25 +254,44 @@ static int suportCount;
 			
 		}
 	}
-	
+	/*
+	 * Calculating Confidence of each rule and placing it in Rule Map 
+	 * if it satisfies the Confidence entered by user
+	 */
 	static void calculateConfidence(Set<Integer>s1,Set<Integer> s2){
 		int s1tos2Count=0;
 		int s2tos1Count=0;
 		int supportCount=0;
-		for(Set<Integer> s:dataSet){
+		getDataBase();
+		try{
+		while(res.next()){
+			String[] str=res.getString(2).split(",");
+			Set<Integer> row=new HashSet<Integer>();
+			for(int i=0;i<str.length;i++){
+				Integer j=Integer.parseInt(str[i]);
+				row.add(j);
+		}
 			Set<Integer>set1=new HashSet<Integer>();
 			Set<Integer>set2=new HashSet<Integer>();
 			set1.addAll(s1);
-			set1.removeAll(s);
+			set1.removeAll(row);
 			if(set1.isEmpty())
 				s1tos2Count++;
 			set2.addAll(s2);
-			set2.removeAll(s);
+			set2.removeAll(row);
 			if(set2.isEmpty())
 				s2tos1Count++;
 			if(set1.isEmpty() && set2.isEmpty())
 				supportCount++;
 			
+		}
+		}catch( Exception e){
+			
+		}
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
 		Float s1tos2Confidence=new Float(supportCount)/new Float(s1tos2Count);
@@ -277,17 +320,28 @@ static int suportCount;
 	   
 	
 	}
-	
+	/*
+	 * Printing Association Rules
+	 */
 	static void getAssociationRules(){
+		if(!associationRules.isEmpty()){
 		System.out.println("\n*************************************************\n");
 		System.out.println("****************Association Rules******************");
 		 for(Set<Integer> key:associationRules.keySet()){
 				for(Set<Integer> value:associationRules.get(key))
 				System.out.println(valueToName(key)+"-->" + valueToName(value) + "\n");
 			}
+		}
+		else
+			{System.out.println("\n**********************************************\n");
+			System.out.println("   *********No Association Rules Generated*******");
+			}
+		
 		
 	}
-	
+	/*
+	 * Getting the name of the Item in Data set
+	 */
 	static String valueToName(Set<Integer> key){
 		String name = "";
 		for(Integer i:key){
@@ -307,7 +361,9 @@ static int suportCount;
 		conf=confidence;
 		confAR.put(Rule, conf);
 	}
-	
+	/*
+	 * Pruning the Supersets of infrequent item sets
+	 */
 	static boolean pruneCandidate(Set<Integer> candidate){
 		Set<Set<Integer>> properSubset=ProperSubset.getProperSubset(candidate.size()-1, candidate);
 		for(Set<Integer> s1:properSubset){
@@ -320,6 +376,27 @@ static int suportCount;
 		
 		return true;
 	}
+	/*
+	 * Making connection to database
+	 */
+	static void getDataBase(){
+		try{
+			res=null;
+			res2=null;
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@prophet.njit.edu:1521:course","cr252","CKHMs7icB");
+			Statement st = con.createStatement();
+			Statement st2=con.createStatement();
+			String sql ="select * from "+CompanyName+" ORDER BY tid";
+			String sql2="select * from "+CompanyName+"_products order by pid";
+			 res=st.executeQuery(sql);
+			 res2=st2.executeQuery(sql2);
+			 
+		}
+			catch(Exception e){System.out.println(e);}
+		
+	}
+	
 	
 }
 
